24d493ef0d06187a15d0baaa5d2a6cefc9841f31
Harsh <harsh20221@gmail.com>
2025-03-29 23:23:55 +0530

    Added fadeout

--- Simon Game Challenge Starting Files/game.js
+++ Simon Game Challenge Starting Files/game.js
@@ -1,1 +1,13 @@
-$('h1').css("color", "blue"); //! The Valid Syntax is .css not .color
+///$('h1').css("color", "blue"); //! The Valid Syntax is .css not .color
+var gamePattern=[]
+var buttoncolors=["red","green","blue","yellow"]
+var randomno = 0;
+function generaterandom() {
+	randomno = Math.floor(Math.random() * 4); // Generates a random integer between 0 and 3
+	return randomno
+}
+var randomChosenColour=buttoncolors[generaterandom()];
+gamePattern.push(randomChosenColour)
+console.log(gamePattern)
+$(`.${randomChosenColour}`).fadeOut(100).fadeIn(100) ////!!!We used . here before $ be3cause out buttons have a class not an id 
+
