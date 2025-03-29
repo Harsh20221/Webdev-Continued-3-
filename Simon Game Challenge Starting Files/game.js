902ead4be95f676dab1e98495ce0f8c4a4b2d356
Harsh <harsh20221@gmail.com>
2025-03-30 00:08:29 +0530

    fixed buttons not popping up issue

--- Simon Game Challenge Starting Files/game.js
+++ Simon Game Challenge Starting Files/game.js
@@ -1,4 +1,5 @@
 ///$('h1').css("color", "blue"); //! The Valid Syntax is .css not .color
+var useraction;
 var gamePattern=[]
 var buttoncolors=["red","green","blue","yellow"]
 var randomno = 0;
@@ -14,3 +15,8 @@ $(`.${randomChosenColour}`).fadeOut(100).fadeIn(100) ////!!!We used . here befor
 
 var playaudio = new Audio(`/Users/harshkumar/Webdev/Webdev Continued-3 /Simon Game Challenge Starting Files/sounds/${randomChosenColour}.mp3`);
 playaudio.play(); 
+
+$('.btn').click(function (event) {  ////!!!If you use ('button) then code will not work because in the html file the buttons arev assigned inside the div
+	useraction = event.target.id;
+	console.log(useraction)
+});
