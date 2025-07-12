a986cfd74fe6ad7de40a917e66492324ed270224
Harsh <harsh20221@gmail.com>
2025-07-12 05:32:52 +0530

    Comments

--- React/React-3/293-keeper-app-part-1-challenge/src/Note.jsx
+++ React/React-3/293-keeper-app-part-1-challenge/src/Note.jsx
@@ -1,5 +1,7 @@
+///!!!!!THIS IS VERY IMPORTANT TO WRITE className="note" because we wanna apply the specific styling that we did on the particular elemets in our css file to our note so we need to include this here 
+///WE ARE Using className here because we are applying the style from the css file , if we were to use  style then it would have been inline style like this -- style={{ background: '#fff',borderRadius: '7px',boxShadow: '0 2px 5px #ccc',padding: '10px',width: '240px', IN SHORT INLINE STYLE IS USED TO APPLY STYLES AT THE JSX FILE ITSELF WHILE CLASSNAME LINKS IT TO THE CSS FILE CREATED EARLIER 
 function Note(){
-    return <div className="note"> ///!!!!!THIS IS VERY IMPORTANT TO WRITE this because we wanna apply the specific styling that we did on the particular elemets to our note so we need to include this here 
+    return <div className="note"> 
         <h1>This is the Note Title</h1>
         <p>This is the Note Preview </p>
     </div>
